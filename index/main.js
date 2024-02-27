803d4f663a53efe695658572bca491afb4cd536b
Wangt0ny <a0922320514@gmail.com>
2024-02-27 19:31:07 +0800

    手機端畫面初版

---
+++ index/main.js
@@ -0,0 +1,362 @@
+let shopMeat = document.getElementById("shop-meat");
+let shopSeafood = document.getElementById("shop-seafood");
+let shopVegetable = document.getElementById("shop-vegetable");
+let shopDumplings = document.getElementById("shop-dumplings");
+
+let meatType = projectDataList.filter((x) => {return x.type === "meat"});
+let seafoodType = projectDataList.filter((x) => {return x.type === "seafood"});
+let vegetableType = projectDataList.filter((x) => {return x.type === "vegetable"});
+let dumplingsType = projectDataList.filter((x) => {return x.type === "dumplings"});
+
+let basket = JSON.parse(localStorage.getItem("data")) || [] ;
+
+//卡片生成模板
+let generateCardMeat = () => {
+    return shopMeat.innerHTML = meatType.map((x) => {//slice選取projectDataList內部分物件
+        let {img, product, price, id} = x;
+        //從本機儲存裡找資料
+        let search = basket.find((x) => x.id === id) || [];
+        return `
+            <div class="col-12 col-lg-3 col-md-6 col-sm-6 mb-3" id=project-id-${id}>
+                <div class="item">
+                        <div class="mycard">
+                            <img class="card-img" src=${img} alt="project-pic"/>
+                            <div class="mycard-body">
+                                <div class="body-info">
+                                    <h4 class="card-title">${product}</h4>
+                                    <p class="card-text">$${price}</p>
+                                </div>
+                                <div class="project-btn-group">
+                                    <i class="bi bi-dash" onclick="decrement(${id})"></i>
+                                    <div id=${id} class="project-count">${search.item === undefined ? 0 : search.item}</div>
+                                    <i class="bi bi-plus" onclick="increment(${id})"></i>
+                                </div>
+                            </div>
+                        </div>   
+                </div>
+            </div>
+        `
+    }).join("")
+
+}
+generateCardMeat();
+
+let generateCardSeafood = () => {
+    return shopSeafood.innerHTML = seafoodType.map((x) => {//slice選取projectDataList內部分物件
+        let {img, product, price, id} = x;
+        //從本機儲存裡找資料
+        let search = basket.find((x) => x.id === id) || [];
+        return `
+            <div class="col-12 col-lg-3 col-md-6 col-sm-6 mb-3" id=project-id-${id}>
+                <div class="item">
+                        <div class="mycard">
+                            <img class="card-img" src=${img} alt="project-pic"/>
+                            <div class="mycard-body">
+                                <div class="body-info">
+                                    <h4 class="card-title">${product}</h4>
+                                    <p class="card-text">$${price}</p>
+                                </div>
+                                <div class="project-btn-group">
+                                    <i class="bi bi-dash" onclick="decrement(${id})"></i>
+                                    <div id=${id} class="project-count">${search.item === undefined ? 0 : search.item}</div>
+                                    <i class="bi bi-plus" onclick="increment(${id})"></i>
+                                </div>
+                            </div>
+                        </div>   
+                </div>
+            </div>
+        `
+    }).join("")
+
+}
+generateCardSeafood();
+
+let generateCardVegetable = () => {
+    return shopVegetable.innerHTML = vegetableType.map((x) => {//slice選取projectDataList內部分物件
+        let {img, product, price, id} = x;
+        //從本機儲存裡找資料
+        let search = basket.find((x) => x.id === id) || [];
+        return `
+            <div class="col-12 col-lg-3 col-md-6 col-sm-6 mb-3" id=project-id-${id}>
+                <div class="item">
+                        <div class="mycard">
+                            <img class="card-img" src=${img} alt="project-pic"/>
+                            <div class="mycard-body">
+                                <div class="body-info">
+                                    <h4 class="card-title">${product}</h4>
+                                    <p class="card-text">$${price}</p>
+                                </div>
+                                <div class="project-btn-group">
+                                    <i class="bi bi-dash" onclick="decrement(${id})"></i>
+                                    <div id=${id} class="project-count">${search.item === undefined ? 0 : search.item}</div>
+                                    <i class="bi bi-plus" onclick="increment(${id})"></i>
+                                </div>
+                            </div>
+                        </div>   
+                </div>
+            </div>
+        `
+    }).join("")
+
+}
+generateCardVegetable();
+
+let generateCardDumplings = () => {
+    return shopDumplings.innerHTML = dumplingsType.map((x) => {//slice選取projectDataList內部分物件
+        let {img, product, price, id} = x;
+        //從本機儲存裡找資料
+        let search = basket.find((x) => x.id === id) || [];
+        return `
+            <div class="col-12 col-lg-3 col-md-6 col-sm-6 mb-3" id=project-id-${id}>
+                <div class="item">
+                        <div class="mycard">
+                            <img class="card-img" src=${img} alt="project-pic"/>
+                            <div class="mycard-body">
+                                <div class="body-info">
+                                    <h4 class="card-title">${product}</h4>
+                                    <p class="card-text">$${price}</p>
+                                </div>
+                                <div class="project-btn-group">
+                                    <i class="bi bi-dash" onclick="decrement(${id})"></i>
+                                    <div id=${id} class="project-count">${search.item === undefined ? 0 : search.item}</div>
+                                    <i class="bi bi-plus" onclick="increment(${id})"></i>
+                                </div>
+                            </div>
+                        </div>   
+                </div>
+            </div>
+        `
+    }).join("")
+
+}
+generateCardDumplings();
+
+
+
+//增量數量函式
+
+let increment = (id) => {
+    // console.log(id);return;
+    let productItem = id;
+
+    let search = basket.find((x) => { return x.id === productItem.id})
+
+    if (search === undefined) {
+        basket.push({
+            id: productItem.id,
+            item: 1,
+        });
+    }
+    else{
+        search.item += 1;
+    }
+    updata(productItem.id);
+    generateCartItem(shoppingCart);
+    generateCartItem(cartContainer);
+    generateCartButton();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+//減量數量函式
+let decrement = (id) => {
+    let productItem = id;
+    
+    let search = basket.find((x) => { return x.id === productItem.id})
+
+
+    if (search === undefined) {
+        return;
+    }
+    else if (search.item === 0) {
+        return;
+    }
+    else{
+        search.item -= 1;
+    }
+    // console.log(basket);
+    updata(productItem.id);
+    //移除數量為0的物件
+    basket = basket.filter((x) => x.item !== 0);//回傳此條件為true的物件
+    generateCartItem(shoppingCart);
+    generateCartItem(cartContainer);
+    generateCartButton();
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
+//更新函式input值
+let updata = (id) => {
+    let search = basket.find((x) => x.id === id);
+    
+    if (search !== undefined) {
+        document.getElementById(id).innerHTML = search.item;
+    }
+    else {
+        document.getElementById(id).innerHTML = 0;
+    }
+    
+    
+    cartTotal();
+    totalPrice();
+    
+    // generateCartItem(shoppingCart);
+};
+
+
+//購物車加總顯示
+let orderCount = document.getElementById("order-count");
+let cartTotal = () => {
+    let itemAmount = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+    orderCount.innerText = itemAmount;
+    // return itemAmount;
+}
+
+cartTotal();
+
+//購物車
+
+let openCart = () => {
+    let cartPage = document.getElementById("cart-page");
+    cartPage.style.transform = "translateX(0%)";
+}
+
+let closeCart = () => {
+    let cartPage = document.getElementById("cart-page");
+    cartPage.style.transform = "translateX(100%)";
+}
+
+let cartTotle = document.getElementById("cart-total");
+let orderPrice = document.getElementById("order-price");
+let shoppingCart = document.getElementById("shopping-cart");
+
+let generateCartItem = (dom) => {
+    //dom參數
+    if (basket.length !== 0){
+        //cart not empty
+        return dom.innerHTML = basket.map((x) => {
+            let {id , item} = x;//物件解構賦值變數
+            
+            let search = projectDataList.find((y) => y.id === id) || [];
+            // console.log(search);
+            return `
+            <div class="cart-item">
+                <img src=${search.img} alt="">
+                <div class="cart-info">
+                    <p class="cart-info-title">${search.product}</p>
+                    <div class="price">
+                        <p>$ ${search.price} x ${item} = ${search.price * item}</p>
+                    </div>
+                </div>
+                <i onclick="removeItem(${id})" class="bi bi-trash"></i>
+            </div>
+            `
+        }).join("")
+    }
+    else {
+        //cart empty
+        cartTotle.innerHTML = ``;
+        dom.innerHTML = `
+        <div class="no-item"><h4>購物車是空的唷</h4></div>
+        `;
+    }
+}
+
+generateCartItem(shoppingCart);
+
+//顯示購物車按鈕
+let generateCartButton = () => {
+    if (basket.length !== 0){
+        document.getElementById("order-info").style.display = "block";
+    }else {
+        document.getElementById("order-info").style.display = "none";
+    }
+}
+generateCartButton();
+
+//開啟購物車
+let cartContainer = document.getElementById("cart-container");
+generateCartItem(cartContainer);
+
+let displayCart = () =>{
+    document.getElementById("order-page").style.transform = "translateY(0%)";
+    console.log("ok");
+    document.querySelector("#order-info > div").setAttribute('onclick',"sendCart()");
+    document.getElementById("order-title").innerHTML = "送出";
+    document.querySelector("#order-info > div > div:nth-child(2)").style.display = "none";
+}
+
+//隱藏購物車
+let hideCart = () => {
+    document.getElementById("order-page").style.transform = "translateY(120%)";
+    document.querySelector("#order-info > div").setAttribute('onclick',"displayCart()");
+    document.getElementById("order-title").innerHTML = "目前";
+    document.querySelector("#order-info > div > div:nth-child(2)").style.display = "block";
+}
+
+//送單
+let sendCart = () => {
+    console.log("sendCart");
+    clearCart();
+}
+
+
+// console.log(document.getElementById("project01"))
+let removeItem = (id) => {
+    let productItem = id;
+    //移除物件
+    basket = basket.filter((x) => x.id !== productItem.id);
+    generateCartItem(shoppingCart);
+    generateCartItem(cartContainer);
+    generateCartButton();
+    cartTotal();
+    updata(productItem.id);
+    localStorage.setItem("data", JSON.stringify(basket));
+}
+
+//購物車金額加總
+let totalPrice = () => {
+    if (basket.length !== 0) {
+        let amount = basket.map((x) => {
+            let {id, item} = x;
+            let search = projectDataList.find((y) => y.id === id) || [];
+            return item * search.price;
+        }).reduce((x, y) => x + y ,0);
+
+        orderPrice.innerHTML = `
+        $ ${amount}
+        `;
+        // return amount;
+    }
+    else {
+        orderPrice.innerHTML = `
+        $ 0
+        `
+    }
+}
+
+totalPrice();
+
+let clearCart = () => {
+    basket = [];
+    generateCartItem(shoppingCart);
+    generateCartItem(cartContainer);
+    generateCartButton();
+    cartTotal();
+    totalPrice();
+    generateCardMeat();
+    generateCardSeafood();
+    generateCardVegetable();
+    generateCardDumplings();
+    
+    localStorage.setItem("data", JSON.stringify(basket)); 
+}
+
+
+//分類錨點
+function scrollToAnchor(anchorName) {
+    var anchor = document.getElementById(anchorName);
+    var offset = anchor.offsetTop;
+    window.scrollTo({
+      top: offset - 150,
+    });
+}
+
